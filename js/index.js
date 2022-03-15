fa5093fb59fdccc6679a28b358ac8b62a2aa97b0
ByeongUk-An <quddnr005@naver.com>
2022-03-16 00:48:43 +0900

    mainpage-complete

--- js/index.js
+++ js/index.js
@@ -4,12 +4,17 @@ const logo = document.querySelector(".logo_img");
 const close = document.querySelector(".close");
 const searchCon = document.querySelector(".search_content");
 const search = document.querySelector(".search");
+const mb = document.querySelector(".mb");
+const searchinput = document.querySelector(".search_input");
+
 document.addEventListener("scroll", function () {
   let currentScrollValue = document.documentElement.scrollTop;
   if (currentScrollValue > 100) {
-    navWrap.classList.add("show");
-    nav.classList.add("show");
-    logo.src = "./asets/images/logo-after.png";
+    if (window.innerWidth >= 993) {
+      navWrap.classList.add("show");
+      nav.classList.add("show");
+      logo.src = "./asets/images/logo-after.png";
+    }
   } else {
     navWrap.classList.remove("show");
     nav.classList.remove("show");
@@ -26,4 +31,29 @@ search.addEventListener("click", function () {
 });
 close.addEventListener("click", function () {
   searchCon.classList.toggle("show");
+  searchinput.value = null;
+});
+mb.addEventListener("click", function () {
+  searchCon.classList.toggle("show");
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+// Hambuger
+const burger = $(".menu-trigger");
+const navclass = $(".nav");
+const logoWrap = $(".logo_wrap");
+
+burger.each(function (index) {
+  const $this = $(this);
+
+  $this.on("click", function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active-2");
+    navclass.toggleClass("show");
+    logoWrap.toggleClass("show");
+  });
 });
